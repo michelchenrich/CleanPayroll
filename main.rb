8c8d60b1d378a576663e130a5553cb32e9c2e902
Henrich <I823377@global.corp.sap>
2016-02-05 11:40:58 -0200

    Heroku setup

---
+++ main.rb
@@ -0,0 +1,80 @@
+require 'sinatra'
+require 'bcrypt'
+require 'data_mapper'
+require 'dm-mysql-adapter'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'mysql://root:root@localhost/CleanPayroll_development')
+
+class User
+  include(DataMapper::Resource)
+  include(BCrypt)
+
+  property(:id, Serial, key: true)
+  property(:username, String, length: 3..50)
+  property(:first_name, String)
+  property(:last_name, String)
+  property(:password, BCryptHash)
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
+
+set :sessions => true
+
+register do
+  def has_any_role?(*roles)
+    condition do
+      redirect '/login' unless has_session?
+    end
+  end
+end
+
+helpers do
+  def display_view(template, model={})
+    erb(template, locals: model)
+  end
+
+  def has_session?
+    @user != nil
+  end
+end
+
+before do
+  @user = User.get(session[:user_id])
+end
+
+get('/') do
+  ee = User.new
+  ee.first_name = 'Michel'
+  ee.last_name = 'Henrich'
+  display_view(:index)
+end
+
+get('/protected', has_any_role?: [:user]) do
+  display_view(:protected)
+end
+
+get('/login') do
+  if has_session?
+    redirect '/'
+  else
+    display_view(:login)
+  end
+end
+post('/login') do
+  user = User.first(username: params[:username])
+  if user && user.password == params[:password]
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+get('/logout') {
+  session[:user_id] = nil
+  redirect '/'
+}
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
